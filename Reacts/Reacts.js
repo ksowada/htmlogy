1de886936d235b77ad958be3fb51604f06aad309
Karl Sowada <karl@sowada.eu>
2023-09-10 19:22:14 +0200

    Reacts non-static

--- Reacts/Reacts.js
+++ Reacts/Reacts.js
@@ -1,4 +1,5 @@
-import {Ids} from '../../Ids'
+import {flatten} from 'lodash-es'
+import Ids from '../../Ids'
 import Obj from '../../Obj/Obj'
 
 /**
@@ -6,28 +7,33 @@ import Obj from '../../Obj/Obj'
  * to generate a map of elements gnerated by hierarchic id
  */
 class Reacts {
-	static ids = {}
-	static els = {}
+	constructor(name) {
+		this.name = name
+		this.ids = {}
+		this.els = {}
+	}
 	/**
 	 * generate id and save hierarchically
 	 * @param {...string|...number} keys set of keys to generate hierarchy
 	 * @returns {string} generated id
 	 */
-	static id(keys) {
-		const id = Ids.combineId(keys)
-		Obj.put(this.ids,keys,id)
+	id(...keys) {
+		const keysFlat = flatten([this.name,keys])
+		const id = Ids.combineId(keysFlat)
+		Obj.put(this.ids,keysFlat,id)
 		return id
 	}
 	/**
 	 * search document for elements
 	 * @returns {object} hierarchy of DOM-elements, according to key-hierarchy given at id generation
 	 */
-	static el() {
+	el() {
 		Obj.mapFunc(this.ids,this.els,id => document.getElementById(id))
 		return this.els
 	}
-	static get(keys) {
-		return Obj.get(this.els,keys)
+	get(...keys) {
+		const keysFlat = flatten([this.name,keys])
+		return Obj.get(this.els,keysFlat)
 	}
 }
 export default Reacts
