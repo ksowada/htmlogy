f0c6c49837dde40f0b65aa82685a0413f1b8a2b1
Karl Sowada <karl@sowada.eu>
2026-08-07 23:12:12 +0200

    Button extends Html

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,63 +1,44 @@
 import Obj from '../../../logic/Obj/Obj.js'
 import Str from '../../../logic/Str/Str.js'
-import HtmlComp from '../../HtmlComp/HtmlComp.js'
 import Html from '../../Html/Html.js'
 import Vars from '../../../logic/Vars/Vars.js'
 import Elem from '../../Elem/Elem.js'
 
 /**
  * @class Button use as HtmlComp
- * use <a> as Button
+ * use <button> as Button, when html is not given,
  * - deactivate by internal state that controls class and click handler
  * - use mouseup as click, use event click for usual click
  * @augments HtmlComp
  */
-class Button extends HtmlComp {
+class Button extends Html {
+	static btnTypes = ['btn-primary','btn-secondary']
+	static disabledTypes = ['enabled','disabled']
+	static mouseState = ['mousedown','mouseup']
+
 	/**
 	 * @param {object} arg parameters for Button construction
 	 * @param {HTMLElement} arg.el DomElement
 	 * @param {string} arg.title a title for Button tooltip
 	 * @param {boolean} arg.disabled disable function and show as disabled
+	 * @param {Function} arg.clickCbk callback for click event
 	 */
-	// TODO search for html:'button'
 	constructor(arg) {
-		super(arg)
-		this.btnTypes = ['btn-primary','btn-secondary']
-		this.disabledTypes = ['enabled','disabled']
-		this.mouseState = ['mousedown','mouseup']
-		Obj.assure(this,'evts',{})
-		this.evts.mousedown = this.mousedown.bind(this)
-		this.evts.mouseup = this.mouseup.bind(this)
-		// this.evts.click = this.click.bind(this)
-		super.constructed()
-	}
-	/**
-	 * @private
-	 */
-	dom() {
-		super.domCreate({disabled:false})
-		let css = Str.listAssure(this.css,' ','btn')
-		css = Str.listAssure(css,' ',this.btnTypes)
-		css = Str.listAssure(css,' ',this.disabledTypes,Vars.boolToInt(this.disabled))
-		Obj.assure(this,'atts',{})
-		if (Str.is1(this.title)) this.atts.title = this.title // Tooltip
-		// if (this.disabled) this.atts.disabled=true // an simple disabled is preferred but dont want to try another hack, besides false setting keeps disabled
-		if (this.blob && this.blobFile) { // TODO try to use only one html-Element instead of a and div
-			// TODO not implemented for update
-			this.a = new Html({parent:this.parent,html:'a'})
-			/** div is root HTMLElement of this item important for List */
-			this.div = this.a.my.el
-			this.btn = new Html({parent:{el:this.a},html:'div',css:css,val:this.val,atts:this.atts})
-			this.a.setAttribute('href',window.URL.createObjectURL(this.blob))
-			this.a.setAttribute('download',this.blobFile)
-			this.a.dataset.downloadurl = [this.blob.type,this.a.download,this.a.href].join(':')
-			this.a.draggable = true
-			this.a.classList.add('dragout')
-		} else {
-			// updated if not already created
-			if (!this.btn) this.btn = new Html({parent:this.parent,html:'button',css:css})
-			else this.btn.change({...this,css:css})
-			this.div = this.btn.my.el
+		Obj.assure(arg,'html','button')
+		Obj.assure(arg,'disabled',false)
+		let css = Str.listAssure(arg.css,' ','btn')
+		css = Str.listAssure(css,' ',Button.btnTypes)
+		css = Str.listAssure(css,' ',Button.disabledTypes,Vars.boolToInt(arg.disabled))
+		Obj.assure(arg,'atts',{})
+		if (Str.is1(arg.title)) arg.atts.title = arg.title // Tooltip
+		Obj.assure(arg,'evts',{})
+		super({...arg,css:css})
+		this.clickCbk = arg.clickCbk
+		this.change({evts:{mousedown:this.mousedown.bind(this),'mouseup':this.mouseup.bind(this)}})
+		// add icon
+		if (arg.icon) {
+			this.add({name:'span',html:'span'})
+			this.span.add({name:'icon',html: 'i',css: arg.icon})
 		}
 	}
 	/**
@@ -71,27 +52,23 @@ class Button extends HtmlComp {
 		if (arg.disabled!==undefined) {
 			Elem.classStateSet(this.btn.my.el,Vars.boolToInt(arg.disabled),this.disabledTypes)
 		}
-		if (this.blob && this.blobFile) {
-			this.a.change(arg)
-		} else {
-			this.btn.change(arg)
-		}
+		this.btn.change(arg)
 		Html.mergeModDatas(this,arg) // at the end remember changed data
 	}
 	// TODO set Timeout for animation, even if its only clicked without long mousedown
 	mousedown(evt) {
-		const el = Elem.findParent(evt.target,this.div) // use parent may show incorrect when directing direct on <i> Logo
+		const el = Elem.findParent(evt.target,this.el) // use parent may show incorrect when directing direct on <i> Logo
 		if (!el) return
 		if (!Elem.classStateIs(el,'disabled')) { // will only change something when enabled
-			Elem.classStateSet(el,'mousedown',this.mouseState)
+			Elem.classStateSet(el,'mousedown',Button.mouseState)
 		}
 	}
 	mouseup(evt) {
-		const el = Elem.findParent(evt.target,this.div)
+		const el = Elem.findParent(evt.target,this.el)
 		if (!el) return
-		Elem.classStateSet(el,'mouseup',this.mouseState)
+		Elem.classStateSet(el,'mouseup',Button.mouseState)
 		if (Elem.classStateIs(el,'enabled')) { // will only change something when enabled
-			if (this.clickCbk) this.clickCbk()
+			if (this.clickCbk) this.clickCbk(this)
 		}
 	}
 }
