685f1ce937d30d82656e0903b029b4d6e3ed860c
Karl Sowada <karl@sowada.eu>
2026-08-14 16:50:05 +0200

    InputInfo getSubVal gets en correctly

--- comp/InputVar/InputInfo.js
+++ comp/InputVar/InputInfo.js
@@ -150,6 +150,7 @@ class InputInfo extends InputVar {
 	getSubVal(key) {
 		if (Object.hasOwn(this.vars_actions, key)) return this.vars_actions[key].get()
 		if (Object.hasOwn(this.vars_subs, key)) return this.vars_subs[key].get()
+		if (key='en') return this.vars['en'].get()
 		return this.get(key)
 	}
 	/**
