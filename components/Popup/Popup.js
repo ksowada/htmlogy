927fbb72407117e4d35b9c27cc4e8474bb6ebb3a
Karl Sowada <karl@sowada.eu>
2026-08-23 20:37:50 +0200

    Popup closes at external click

--- components/Popup/Popup.js
+++ components/Popup/Popup.js
@@ -8,7 +8,7 @@ class Popup extends Html {
 	/**
 	 * Creates a popup component.
 	 * @param {object} arg Popup configuration.
-   * @param {Array} items Html attributes for each item
+	 * @param {Array} items Html attributes for each item
 	 */
 	constructor(arg,items) {
 		super(Html.mergeDatas(arg,{css:'popup'}))
@@ -18,17 +18,21 @@ class Popup extends Html {
 				console.log('Popup.constructor.item:',item.val)
 			})
 		}
-		// };
-		// Schließen bei Klick außerhalb
-		// document.addEventListener("click", (evt) => {
-		// if (!this.el.contains(evt.target)) {
-		// 	this.close();
-		// }
-		// });
+		// Schließen bei Klick außerhalb, first click is from opening, therefore ignoreNextClick
+		this.ignoreFirstClick = true
+		document.addEventListener('click',evt => {
+			console.log('document.click:',evt)
+			if (this.ignoreFirstClick) {
+				this.ignoreFirstClick = false
+				return // Öffnungs-Klick ignorieren
+			}
+			if (!this.el.contains(evt.target)) {
+				this.close()
+			}
+		})
 	}
-	// close() {
-	// 	this.remove()
-	// }
-
+	close() {
+		this.remove()
+	}
 }
 export default Popup
